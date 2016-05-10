736419c42b70ba1aedcd1dabbf06661d664888d6
KuroOnee <Dim22Rus@gmail.com>
2016-05-10 10:57:44 +0600

    Fixed guards

--- src/def.h
+++ src/def.h
@@ -1,37 +1,18 @@
 #include <stdio.h>
+#ifndef def_h
+#define def_h
 #ifndef n
 #define n 100
 #endif
-#ifndef input
 void input(char str[n]);
-#endif
-#ifndef check
 void check(char str[n],char str2[n],int* a,int* b);
-#endif
-#ifndef process
 void process(char str1[n]);
-#endif
-#ifndef output
 int output(int s,int x,char str[n],char str1[n]);
-#endif
-#ifndef stok
 void stok(char str[n],char str2[n]);
-#endif
-#ifndef toLowCase
 void toLowCase(char s[n]);
-#endif
-#ifndef sequal
 int sequal(char s1[n], char t[n]);
-#endif
-#ifndef slen
 int slen(char s[n]);
-#endif
-#ifndef sspn
 int sspn(char str[n],int k);
-#endif
-#ifndef scut
 void scut(char s2[n],char s[n],char cut);
-#endif
-#ifndef scopy
 void scopy(char s1[n],char s2[n]);
 #endif
